57b58cb2fee86735f0fc5f66057c86d6f65ca895
TeklaTevzadze <tekla.tevzadze@geolab.edu.ge>
2019-07-27 00:14:12 -0700

    MainLayout

--- LittleBooks/Style/js/script.js
+++ LittleBooks/Style/js/script.js
@@ -15,33 +15,38 @@
 $(".photo").click(function () {
     $(".search").toggleClass("active")
 });
-$(document).ready(function () {
-    $('.gallery').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 470,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+$(".arrow-down").click(function () {
+  $(".arrow-down").toggleClass("rotate")
+  $(".hide").toggleClass("shown")
+  $(".text-hide").toggleClass("text-shown")
 });
+$(document).ready(function(){
+		  $('.gallery').slick({
+			centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 470,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+			});
+		});
